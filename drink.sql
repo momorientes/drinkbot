63a5599e3013bb31e8bb289233c4db38aa7fddf1
momo <m@hackathon.de>
2013-07-02 03:43:37 +0200

    fixing sql syntax

--- drink.sql
+++ drink.sql
@@ -1,3 +1,3 @@
-CREATE TABLE users(user_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, active_days INTEGER DEFAULT NULL, drinks_total FLOAT DEFAULT NULL) 
-CREATE TABLE drinks_today(user_id INTEGER, FOREIGN KEY(user_id) REFERENCES users(user_id), amount FLOAT, timestamp INTEGER)
-CREATE TABLE drinks_archive(user_id INTEGER, FOREIGN KEY(user_id) REFERENCES users(user_id), amount FLOAT, timestamp INTEGER)
+CREATE TABLE users(user_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, active_days INTEGER DEFAULT NULL, drinks_total FLOAT DEFAULT NULL); 
+CREATE TABLE drinks_today(user_id INTEGER, amount FLOAT, timestamp INTEGER, FOREIGN KEY(user_id) REFERENCES users(user_id));
+CREATE TABLE drinks_archive(user_id INTEGER, amount FLOAT, timestamp INTEGER, FOREIGN KEY(user_id) REFERENCES users(user_id));
