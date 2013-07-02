aa2838083af6b2b259421791c63e340a89f88668
rixx <rike.kunze@gmail.com>
2013-07-02 05:27:19 +0200

    dailyReset

--- dailyReset.py
+++ dailyReset.py
@@ -3,8 +3,22 @@
 
 import sqlite3
 
+#open connection
 con = sqlite3.connect('drink.db')
 cur = con.cursor()
-cur.execute('DROP TABLE Daily')
+
+#get all entered userIDs and cumulated drinks from the past day
+cur.execute('SELECT user_id, drink_sum FROM (SELECT sum(amount) AS drink_sum, user_id FROM drinks_today GROUP BY user_id) ORDER BY drink_sum DESC')
+daily_totals = cur.fetchall()
+
+#for every user: increment active days, add to total drinks
+for user in daily_totals:
+    cur.execute('UPDATE users SET active_days = active_days + 1, drinks_total = drinks_total + ? WHERE user_id = ?',(user[1],user[0]))
+
+#move drinks_today to drinks_archive
+cur.execute('SELECT * INTO drinks_archive FROM drinks_today')
+cur.execute('DELETE FROM drinks_today')
+
+#aaaand bye
 con.commit()
 con.close()
